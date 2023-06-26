450d44315bcba46075aec346efbf0706cede2034
cleversamerr <thedev.samer@gmail.com>
2023-06-26 09:43:00 +0300

    added store items

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 sanitize(app);
 
+const storeItems = new Map([
+  [1, { priceInCents: 10000, name: "Item 1" }],
+  [2, { priceInCents: 15000, name: "Item 2" }],
+]);
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
